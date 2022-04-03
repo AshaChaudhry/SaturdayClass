4dc19b109cb08d7cc74f17108af1c180d73f6f8d
unknown <ash.chaudhry@yahoo.co.uk>
2022-04-03 19:06:17 +0100

    upadated file

---
+++ Testingtopics.java
@@ -0,0 +1,53 @@
+package com.learning.manualtesting;
+
+public class Testingtopics {
+
+    public static void main(String[] args) {
+     //topics fo week 1
+        System.out.println("* what is software?");
+        System.out.println("* Software risks");
+        System.out.println("* what is software testing and benefits");
+        System.out.println("* What is Manual Testing");
+        System.out.println("* Who does Testing?");
+        System.out.println("* SDLC AND STLC");
+
+
+// topics for week 2
+        System.out.println("* Challenges of Manual Testing");
+        System.out.println("* Principals of Software Testing");
+        System.out.println("*  Plan and Template");
+        System.out.println("* Test Scenario and Template");
+        System.out.println("* Test case and Test case template");
+        System.out.println("* Test scenario vs Test case");
+
+        //topics for week 3
+        System.out.println("* Testing Techniques");
+        System.out.println("* What is a bug?");
+        System.out.println("* Bug lifecycle");
+        System.out.println("* Bug Template");
+        System.out.println("* Severity vs Priority");
+        System.out.println("* Defect Tracking Tool JIRA");
+
+        //topics for week 4
+        System.out.println("* Testing levels- White box testing and Black box tesing");
+        System.out.println("* Static vs Dynamic");
+        System.out.println("* Exploratory testing");
+        System.out.println("* Ad hoc testing");
+        System.out.println("* software development models-");
+        System.out.println("       -Waterfall model");
+        System.out.println("       -V model(Verification and validation)");
+        System.out.println("       -Iterativ model");
+        System.out.println("       -Agile model");
+        System.out.println("* defination of done and ready");
+
+        //topics for week 5
+        System.out.println("* what is TDD?");
+        System.out.println("* what is BDD?");
+        System.out.println("* BDD keywords");
+        System.out.println("* how to write BDD test cases and maintenance");
+
+
+// end of Manual testing
+
+    }
+}
